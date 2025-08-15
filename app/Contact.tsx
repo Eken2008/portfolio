7a7f035d148399660df10398ab39ea93457f94ec
Eken2008 <oskar.eklov@gmail.com>
2025-08-15 18:15:08 +0200

    Fix mail button open in new tab

--- app/Contact.tsx
+++ app/Contact.tsx
@@ -4,7 +4,7 @@ export function Contact() {
             <div className="container" style={{paddingBottom: "100px"}}>
                 <h1>Contact</h1>
                 <div style={{display: "flex", justifyContent: "center"}}>
-                    <a className="button projectLink" href="mailto:oskar.eklov@gmail.com">Email - oskar.eklov@gmail.com</a>
+                    <a className="button projectLink" href="mailto:oskar.eklov@gmail.com" target="_blank" rel="noopener noreferrer">Email - oskar.eklov@gmail.com</a>
                     <a className="button githubLink" href="https://github.com/Eken2008" target="_blank" rel="noopener noreferrer"><img src="/img/github.svg" alt="github logo" />GitHub - Eken2008</a>
                     <a className="button linkedinLink" href="https://www.linkedin.com/in/oskar-eklov" target="_blank" rel="noopener noreferrer"><img src="/img/linkedin.png" alt="github logo" />Linkedin - Oskar Eklöv</a>
                 </div>
